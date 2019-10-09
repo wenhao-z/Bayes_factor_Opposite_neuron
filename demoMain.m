5e9c322f7afa18563296bb634cc9853015b31ad3
Wenhao Zhang <wenhaoz1@andrew.cmu.edu>
2019-10-09 12:12:23 -0400

    Update demoMain.m

--- demoMain.m
+++ demoMain.m
@@ -14,27 +14,23 @@ flagDemo = 2;
 % 2. Plot the Bayes factor as varing STRENGTH of two cues
 % 3. The decision boundary between integration and segregation models with 
 %    input spike counts.      
-% 4. Plot the reconstructed likelihood
-% 5. Verify the approximation of marginal model evidence is good
-% 6. Decode the posterior and Bayes factor from congruent and opposite
+% 4. Verify the approximation of marginal model evidence is good
+% 5. Decode the posterior and Bayes factor from congruent and opposite
 %    neurons, and compare with theoretical predictions.
-% 7. Plot the tuning of an example congruent and opposite neurons
+% 6. Plot the tuning of an example congruent and opposite neurons
 
 switch flagDemo
     case 1
-        %         demoBayesFactor_Angle;
         demoBFx_Angle;
     case 2
         demoBayesFactor_Lambda;
     case 3
         DecisionBoundary_Lambda;
     case 4
-        demoBestFitLikelihood;
-    case 5
         cmprMdlEvidenceMargin;
-    case 6
+    case 5
         decodeBayesFactor;
-    case 7 
+    case 6 
         plotCONeuronTuning;
 end
 
